7897edb5fd9f27ac4161ddd70ae3903ab2209371
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-11-13 11:27:29 +0100

    Import Hero into List

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Hero.scss';
 
-const Hero = () => {
-  <header className={styles.component}>
-    <h2 className={styles.title}>Things to do</h2>
-    <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}/>
+const Hero = () => (
+  <header class={styles.component}>
+    <h2 class={styles.title}>Things to do</h2>
+    <img src="https://i.ibb.co/x863rML/black-and-white-blank-challenge-connect-262488-1.jpg" class={styles.image} />
   </header>
-};
+);
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,7 @@ class List extends React.Component {
   render() {
     return(
       <section className={styles.component}>
-        <h2>Things to do</h2>
+        <Hero />
       </section>
     )
   }
